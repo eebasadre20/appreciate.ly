a7152310a47613c77101ab29b82c24626dd8f292
Edsil Basadre <eebasadre20@gmail.com>
2017-07-14 23:08:09 +0800

    added(): Improve some layout on login and home. lol.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def email_initial
+    email.first.upcase
+  end
+
+  def username
+    email.partition('@').first.capitalize
+  end
 end
