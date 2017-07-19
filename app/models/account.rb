7b15b9083156835feb04464f0f72db5a60e4bd3d
Edsil Basadre <eebasadre20@gmail.com>
2017-07-20 00:25:59 +0800

    added(): Partial db design for user and account model.

---
+++ app/models/account.rb
@@ -0,0 +1,4 @@
+class Account < ApplicationRecord
+  has_many :account_members
+  has_many :users, through: :account_members
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_one :account_member
+  has_one :account, through: :account_member
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 
   def email_initial
     email.first.upcase
