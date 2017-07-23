4d0f0326c5542aaa2e2ac5c8c50835c250332ede
Pepeng Agimat <eebasadre20@gmail.com>
2017-07-23 15:21:50 +0800

    added(): Points relationship between user who give and receive. (#4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   has_one :account_member
   has_one :account, through: :account_member
+
+  has_many :received_points, class_name: 'PointsRelationship',
+                           foreign_key: 'giver_id',
+                           dependent: :destroy
+
+  has_many :given_points, class_name: 'PointsRelationship',
+                           foreign_key: 'receiver_id',
+                           dependent: :destroy
+                
+  has_many :givers, through: :given_points, source: :giver
+  has_many :receivers, through: :received_points, source: :receiver
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719160421) do
+ActiveRecord::Schema.define(version: 20170723061048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20170719160421) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "points_relationships", force: :cascade do |t|
+    t.integer "giver_id"
+    t.integer "receiver_id"
+    t.integer "points"
+    t.index ["giver_id"], name: "index_points_relationships_on_giver_id"
+    t.index ["receiver_id"], name: "index_points_relationships_on_receiver_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,6 +53,9 @@ ActiveRecord::Schema.define(version: 20170719160421) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
